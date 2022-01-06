8dfcc7597c2aac6ade0387eb998594450e034c16
sch01957 <git-andreas@vier-schmidts.de>
2022-01-06 13:07:48 +0100

    https for django running

--- projectBlog/projectBlog/settings.py
+++ projectBlog/projectBlog/settings.py
@@ -39,6 +39,8 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'django_extensions',
+    'sslserver',
     'microblog.apps.MicroblogConfig',
     'markdownify.apps.MarkdownifyConfig',
     'telegramBot.apps.TelegrambotConfig',
---
+++ startDjangoServerCert.cmd
@@ -0,0 +1,3 @@
+cd projectBlog
+python manage.py runserver_plus --cert-file localhost+1.pem --key-file localhost+1-key.pem
+pause
